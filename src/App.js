b40e014ed90d2d7005e2a687654a75c466a9338f
Fady-Alwazir <fady.alwazir@gmail.com>
2022-12-07 13:46:52 +0200

    customize the layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Button } from 'antd';
-import { ConfigProvider } from 'antd';
 
 function App() {
   return (
     <div>
       <h1>test</h1>
-      <Button type="secondary">Button</Button>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
